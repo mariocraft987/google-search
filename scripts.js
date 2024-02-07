9d8e7644471cade96bead1f72d27aa109e2995a6
Mariocraft987 <154646419+Mariocraft987@users.noreply.github.com>
2024-02-07 12:47:31 -0700

    q=done

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
+
+let demo = '<br/><img src="img/logoGoogle.jpg" draggable="false" width="240" >';
+demo += '<select name="searchs"><option>10 results</option><option>30 results</option><option>100 results</option></select>';
+demo += '<hr><p style="text-align: center; color: gray;">Showing 1-10 results that apeared in 0.03 seconds</p>'
+
 function lucky() {
-    document.getElementById('content').innerHTML = "";
+    /* demo += '<a href="https://en.wikipedia.org/wiki/Dog">Dogs (wiki)</a>'
+    demo += '<h6>The dog is a domesticated descendant of the wolf. Also called<br/> the domestic dog, it is derived from extinct gray wolves, and the gray wolf is the dogs...</h6>'
+    demo += '<a href="https://en.wikipedia.org/wiki/Cats">Cats (wiki)</a>'
+    demo += '<h6>The cat (Felis catus), commonly referred to as the<br/> domestic cat or house cat, is the only domesticated species in the family Felidae.</h6>' */
+    document.getElementById('content').innerHTML = demo;
     document.getElementById('url').innerHTML = "http://www.google.com/feelinglucky";
 }
 
 function search() {
     let input = document.getElementById('input').value
-    document.getElementById('content').innerHTML = "";
+    if (input != "") {
+    document.getElementById('content').innerHTML = demo;
     document.getElementById('url').innerHTML = "http://www.google.com/search?q=" + input
     .replaceAll(" ", "+");
+    }
 }
