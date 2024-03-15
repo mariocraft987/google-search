ea43dbeaf62a89cd41e7008551d1299c03d8986a
Mr. Rufflesman <154646419+mariocraft987@users.noreply.github.com>
2024-03-14 17:37:14 -0700

    Create site_example.js

---
+++ static/site_example.js
@@ -0,0 +1,9 @@
+{
+  search: 'example site'
+  showing: true,
+  site_url: 'example.com',
+  site_name: 'Example Website',
+  site_description; 'This is an example site.',
+  creator: 'mariocraft987',
+  site_content_url: 'https://examplesite.net'
+}
