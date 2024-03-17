d3cc36d8da6f543bee5ab341811784d30679d21c
Jolly Young Man <154646419+mariocraft987@users.noreply.github.com>
2024-03-16 17:02:56 -0700

    forget it

--- src/scripts.js
+++ src/scripts.js
@@ -20,14 +20,3 @@ function search() {
     .replaceAll(" ", "+");
     }
 }
-
-const contextMenu = document.getElementById("context-menu");
-const scope = document.querySelector("body");
-
-scope.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-
-  const { clientX: mouseX, clientY: mouseY } = event;
-
-  contextMenu.style.top = `${mouseY}px`;
-  contextMenu.style.left = `${mouseX}px`;
